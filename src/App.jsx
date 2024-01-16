0baa1b503d207306cf2b04a079edc85946c67b46
mausamigithub12 <mausamiadhikari4@gmail.com>
2024-01-16 09:17:02 +0545

    here

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    
+      <div className='bg-green-500 inset-y-0 w-2/4' >
+        Mausami
+        
+    </div>
+  )
+}
+
+export default App
